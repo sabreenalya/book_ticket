2241ea29cda5a3c239bd37e7398b5905662345b6
sabreenalya <sabrina.nas26@gmail.com>
2025-04-10 02:24:58 +0800

    Add bottom nav bar and home screen; update main and pubspec

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../res/styles/app_styles.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Good Morning", style: AppStyles.headLineStyle3),
+                        const SizedBox(height: 5),
+                        Text("Book Tickets", style: AppStyles.headLineStyle1),
+                      ],
+                    ),
+                    Container(width: 50, height: 50,decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10), image: DecorationImage(image: AssetImage("assets/images/logo.png"))
+                    ),),
+                  ],
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text("Search Icon"), Text("Empty Space")],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
